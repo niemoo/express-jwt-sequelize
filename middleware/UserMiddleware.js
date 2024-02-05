4b3e6c12c4f7a5bed4e15e6c1b9e609caf198437
Izzan Abdul <izzanabdul123@gmail.com>
2024-02-05 20:25:47 +0700

    feat: add auth jwt

---
+++ middleware/UserMiddleware.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const authMiddleware = async (req, res, next) => {
+  let token;
+
+  // TO CHECK THERE IS A TOKEN OR NOT IN THE HEADER
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      res.status(401).json({
+        status: 401,
+        message: 'Invalid token, please login first',
+      })
+    );
+  }
+
+  let decoded;
+
+  // TO DECODE VERIFICATION TOKEN
+  try {
+    decoded = await jwt.verify(token, process.env.JWT_SECRET);
+  } catch (err) {
+    return next(
+      res.status(401).json({
+        error: err,
+        message: 'Invalid token',
+      })
+    );
+  }
+
+  // GET USER DATA BY DECODED CONDITION
+  const currentUser = await User.findByPk(decoded.id);
+  console.log(currentUser);
+
+  req.user;
+
+  next();
+};
+
+module.exports = authMiddleware;
--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const categories = require('../controllers/categoryControllers');
+const authMiddleware = require('../middleware/UserMiddleware');
 const router = express.Router();
 
 router.get('/', categories.getAllCategoriesData);
-router.get('/:id', categories.getDetailCategoriesData);
+router.get('/:id', authMiddleware, categories.getDetailCategoriesData);
 router.post('/add', categories.addCategoriesData);
 router.put('/update', categories.updateCategoriesData);
 
